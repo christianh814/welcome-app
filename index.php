fa53707662365bf667b8268e8ff53c5291f12811
Christian Hernandez <christian.hernandez@yahoo.com>
2021-03-16 16:28:41 -0700

    updated

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift!<br /><h2>Have a WONDERFUL day!</h2></h1>
+            <h1>PHP application on OpenShift!<br /><h2>Have a SWEET day!</h2></h1>
           </hgroup>
 
 <?php
