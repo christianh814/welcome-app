6e54f5373b054d387af07280e2d2754557bb87b5
Christian Hernandez <chernand@redhat.com>
2021-04-12 17:21:59 -0700

    testing kustomize 5

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift!<br /><h2>Kustomize take 4</h2></h1>
+            <h1>PHP application on OpenShift!<br /><h2>Kustomize take 5</h2></h1>
           </hgroup>
 
 <?php
