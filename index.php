074ec53a21a89005ad20f0c1bbe443ae97947c53
Christian Hernandez <chernand@redhat.com>
2021-04-12 18:33:57 -0700

    updated index

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift!<br /><h2>Test application for OpenShift</h2></h1>
+            <h1>PHP application on OpenShift!<br /><h2>Test application for OpenShift.</h2></h1>
           </hgroup>
 
 <?php
