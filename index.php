dc8aae3c636584d9fe42840c0cacab200b9f00eb
Christian Hernandez <chernand@redhat.com>
2021-04-23 14:49:58 -0700

    updated blue

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift!<br /><h2>GREEN.</h2></h1>
+            <h1>PHP application on OpenShift!<br /><h2>BLUE.</h2></h1>
           </hgroup>
 
 <?php
